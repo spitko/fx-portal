6d9e73ab290d0c52ebd5f9339f516b1bc69dc357
Samuel Pitko <samuel.pitko@gmail.com>
2024-04-27 13:50:53 +0300

    spring initializr

---
+++ src/main/java/ee/pitko/fx/FxApplication.java
@@ -0,0 +1,13 @@
+package ee.pitko.fx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/pitko/fx/FxApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.pitko.fx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
