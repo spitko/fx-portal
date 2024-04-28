dcb9a12405b60dc86889f857f8b5cfcc6813c995
Samuel Pitko <samuel.pitko@gmail.com>
2024-04-28 22:44:55 +0300

    add cors

--- src/main/java/ee/pitko/fx/controller/RateController.java
+++ src/main/java/ee/pitko/fx/controller/RateController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/rates")
 public class RateController {
